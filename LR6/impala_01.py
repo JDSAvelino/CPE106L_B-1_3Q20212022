5441c176bf123d78321a62b8aa18e3a1672e3910
Leon Abraham Apit <100175621+Leon-Abraham@users.noreply.github.com>
2022-05-02 00:34:41 +0800

    Add files via upload

---
+++ LR6/impala_01.py
@@ -0,0 +1,8 @@
+from impala.dbapi import connect
+conn = connect(host='localhost', port=10000)
+cursor = conn.cursor()
+cursor.execute('SELECT * FROM customer LIMIT 100')
+# print (cursor.description) # prints the result set's schema
+# results = cursor.fetchall()
+for row in cursor:
+ print(row)
